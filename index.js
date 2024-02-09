809a91d0727a85f72cf95292d9de3e7540cd813c
Nolan <officialstackemup@gmail.com>
2024-02-08 19:26:48 -0500

    up

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 // make a node module  program using regex to make every vaild type of version's, from 0.0.1 to 999.999.999, and add them to a file called validVer.json, each version should be on a new line.
 // Include Alpha, Beta, Snap and Dev versions. under 1.0.0
 // The version should be in the format "major.minor.patch"
+// include main release versions, and pre-release versions as mentioned above.
 
 const fs = require('fs');
-const path = require('path');
 
-module.exports = function() {
-    const validVer = [];
-    for (let major = 0; major < 1000; major++) {
-        for (let minor = 0; minor < 1000; minor++) {
-        for (let patch = 0; patch < 1000; patch++) {
-            if (major < 1 && minor < 1 && patch < 1) {
-            validVer.push(`${major}.${minor}.${patch}-Alpha`);
-            validVer.push(`${major}.${minor}.${patch}-Beta`);
-            validVer.push(`${major}.${minor}.${patch}-Snap`);
-            validVer.push(`${major}.${minor}.${patch}-Dev`);
+const validVer = [];
+
+module.exports = function () {
+    for (let i = 0; i < 1000; i++) {
+        for (let j = 0; j < 1000; j++) {
+        for (let k = 0; k < 1000; k++) {
+            if (i < 1) {
+            validVer.push(`${i}.${j}.${k}-Alpha`);
+            validVer.push(`${i}.${j}.${k}-Beta`);
+            validVer.push(`${i}.${j}.${k}-Snap`);
+            validVer.push(`${i}.${j}.${k}-Dev`);
             } else {
-            validVer.push(`${major}.${minor}.${patch}`);
+            validVer.push(`${i}.${j}.${k}`);
             }
         }
         }
     }
-    
-    fs.writeFileSync(path.join(__dirname, 'validVer.json'), JSON.stringify(validVer, null, 2));
+    fs.writeFileSync('validVer.json', JSON.stringify(validVer));
     };
 
-    module.exports();
+module.exports();
 
